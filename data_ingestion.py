33adcdf3ac9bedda4bb327b70501915682ed8d82
Bandi Bharath <bandibharath34@gmail.com>
2026-07-25 14:51:12 +0530

    Day 1: Mutual Fund Analytics completed

---
+++ data_ingestion.py
@@ -0,0 +1,76 @@
+import os
+import pandas as pd
+
+# Path to raw data
+folder = "data/raw"
+
+# Get all CSV files
+csv_files = [f for f in os.listdir(folder) if f.endswith(".csv")]
+
+print(f"\nTotal CSV files found: {len(csv_files)}\n")
+
+for file in csv_files:
+    path = os.path.join(folder, file)
+
+    print("=" * 70)
+    print(f"File: {file}")
+    print("=" * 70)
+
+    try:
+        df = pd.read_csv(path)
+
+        print("Shape:")
+        print(df.shape)
+
+        print("\nData Types:")
+        print(df.dtypes)
+
+        print("\nFirst 5 Rows:")
+        print(df.head())
+
+        print("\nMissing Values:")
+        print(df.isnull().sum())
+
+        print("\nDuplicate Rows:")
+        print(df.duplicated().sum())
+
+       
+
+    except Exception as e:
+        print(f"Error reading {file}: {e}")
+
+    print("\n")
+
+    # Store fund master dataset
+fund_master = pd.read_csv("data/raw/01_fund_master.csv")
+
+print("\n========== UNIQUE FUND HOUSES ==========")
+print(fund_master["fund_house"].unique())
+
+print("\n========== UNIQUE CATEGORIES ==========")
+print(fund_master["category"].unique())
+
+print("\n========== UNIQUE SUB CATEGORIES ==========")
+print(fund_master["sub_category"].unique())
+
+print("\n========== UNIQUE RISK CATEGORIES ==========")
+print(fund_master["risk_category"].unique())
+
+# Read datasets
+fund_master = pd.read_csv("data/raw/01_fund_master.csv")
+nav_history = pd.read_csv("data/raw/02_nav_history.csv")
+
+# Get AMFI codes
+fund_codes = set(fund_master["amfi_code"])
+nav_codes = set(nav_history["amfi_code"])
+
+# Find missing codes
+missing_codes = fund_codes - nav_codes
+
+print("\n========== AMFI CODE VALIDATION ==========")
+
+if len(missing_codes) == 0:
+    print("✅ All AMFI codes from Fund Master are present in NAV History.")
+else:
+    print("❌ Missing AMFI Codes:")
+    print(missing_codes)
---
+++ live_nav_fetch.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import pandas as pd
+
+schemes = {
+    "HDFC_Top100": 125497,
+    "SBI_Bluechip": 119551,
+    "ICICI_Bluechip": 120503,
+    "Nippon_LargeCap": 118632,
+    "Axis_Bluechip": 119092,
+    "Kotak_Bluechip": 120841
+}
+
+os.makedirs("data/raw/live_nav", exist_ok=True)
+
+for name, code in schemes.items():
+    print(f"Fetching {name}...")
+
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()["data"]
+        df = pd.DataFrame(data)
+
+        df.to_csv(f"data/raw/live_nav/{name}.csv", index=False)
+
+        print(f"✅ {name} saved successfully.")
+    else:
+        print(f"❌ Failed to fetch {name}")
